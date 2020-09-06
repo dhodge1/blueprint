7fcd1a6cdf430c8b2fe7a620de5e53d17bd789c5
dhodge1 <David.Hodge@scrippsnetworks.com>
2020-09-06 17:19:10 -0400

    Adds timer to schedule page

--- src/pages/kitchen/class-schedule.js
+++ src/pages/kitchen/class-schedule.js
@@ -1,5 +1,15 @@
-const ClassSchedule = () => {
-  return <p>Class Schedule</p>;
+/* eslint-disable react/prop-types */
+export async function getStaticProps() {
+  return {
+    props: {
+      now: new Date().toGMTString(),
+    },
+    revalidate: 1,
+  };
+}
+
+const ClassSchedule = ({ now }) => {
+  return <p>Class Schedule {now}</p>;
 };
 
 export default ClassSchedule;
